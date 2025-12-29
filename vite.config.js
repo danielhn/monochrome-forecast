749dcd7423fae10a53d3bf35444f5551c3f4c82b
danielhn <git-commits@danielhn.com>
2025-12-29 11:57:32 +0100

    Update vite to 7.3.0

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
                     'global-builtin',
                     'legacy-js-api',
                 ],
+                quietDeps: true
             },
         },
     },
